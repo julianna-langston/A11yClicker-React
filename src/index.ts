9e628f1b08ee922264a081f132e69fc2f47e35ed
Julianna Langston <julianna@cosaic.io>
2022-12-05 09:28:06 -0500

    Add a11yClickerFn

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import A11yClicker from './A11yClicker';
+import {a11yClickProps} from "./a11yClickFn";
 
-export {A11yClicker};
+export {A11yClicker, a11yClickProps};
--- test/A11yClicker.test.tsx
+++ test/A11yClicker.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {render, fireEvent} from "@testing-library/react";
-import {A11yClicker} from "../src";
+import {A11yClicker, a11yClickProps} from "../src";
 
 describe("A11yClicker", () => {
     test("A11yClicker merely modifies the child", async () => {
@@ -57,4 +57,57 @@ describe("A11yClicker", () => {
         });
         expect(clickCatcher.mock.calls.length).toBe(3);
     });
-})
+});
+
+describe("a11yClickProps", () => {
+    test("A11yClicker merely modifies the caller", async () => {
+        const clickCatcher = jest.fn();
+        const {container} = render(
+        <div id="root">
+            <div id="myObj" {...a11yClickProps(clickCatcher)}>Hello world</div>
+        </div>);
+
+        // "Root" is the first child of the rendered doc
+        const myObj = container.querySelector("#root > #myObj");
+        expect(myObj).not.toBeNull();
+
+        expect(myObj?.getAttribute("tabIndex")).toBe("0");
+    });
+    test("Div with click handler", async () => {
+        const clickCatcher = jest.fn();
+        const {container} = render(
+            <div id="myObj" {...a11yClickProps(clickCatcher)}>Hello world</div>
+        );
+
+        // Element was built
+        expect(container.outerHTML).not.toBeNull();
+
+        // Mock function hasn't been called
+        expect(clickCatcher.mock.calls.length).toBe(0);
+
+        // Click on element
+        const myObj = container.querySelector("#myObj") as HTMLElement;
+        fireEvent.click(myObj);
+
+        // Confirm function was called when element was clicked
+        expect(clickCatcher.mock.calls.length).toBe(1);
+
+        fireEvent.keyDown(myObj, {
+            key: "Enter",
+            preventDefault: () => {}
+        });
+        expect(clickCatcher.mock.calls.length).toBe(2);
+
+        fireEvent.keyDown(myObj, {
+            key: " ",
+            preventDefault: () => {}
+        });
+        expect(clickCatcher.mock.calls.length).toBe(3);
+
+        fireEvent.keyDown(myObj, {
+            key: "a",
+            preventDefault: () => {}
+        });
+        expect(clickCatcher.mock.calls.length).toBe(3);
+    });
+});
